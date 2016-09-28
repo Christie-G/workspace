3c7121ae7a14e052ccb624a97b5e8e80742d15ac
Christie-G <christiegriffith@gmail.com>
2016-09-27 23:11:58 -0700

    added setting button and project name to nav bar

--- js/imagescard.js
+++ js/imagescard.js
@@ -1,11 +1,10 @@
 $(document).on('click', '#imagesLaunch', function () {
-    $("#workspace").append('<div class="card narrow"id="imagescard" draggable=true><div class="card-header" id="imagesHeader">Images<a href="#imageblock" data-toggle="collapse" aria-expanded="true"><i class="fa fa-compress pull-right" aria-hidden="true"></i></a><a href="#" id="closeimages"><i class="fa fa-times pull-right" aria-hidden="true"></i></a></div><div class="card-block collapse in" id="imageblock"><img src="http://lorempicsum.com/up/400/200/2" class="img-fluid"><img src="http://lorempicsum.com/nemo/400/200/1" class="img-fluid"><img src= "http://lorempicsum.com/up/400/200/2" class="img-fluid"><img src= "http://lorempicsum.com/nemo/400/200/1" class="img-fluid"></div></div>');
+    $("#workspace").append('<div class="card narrow"id="imagescard" draggable=true><div class="card-header" id="imagesHeader">Images<a href="#imageblock" data-toggle="collapse" aria-expanded="true"><i class="fa fa-compress pull-right" aria-hidden="true"></i></a><a href="#" id="closeimages"><i class="fa fa-times pull-right" aria-hidden="true"></i></a></div><div class="card-block collapse in" id="imageblock"><img src="http://lorempicsum.com/up/400/200/2" class="img-fluid imglist"><img src="http://lorempicsum.com/nemo/400/200/1" class="img-fluid imglist"><img src= "http://lorempicsum.com/up/400/200/2" class="img-fluid imglist"><img src= "http://lorempicsum.com/nemo/400/200/1" class="img-fluid imglist"></div></div>');
     Draggable.create(".card", {
         type: "top,left",
         edgeResistance: 0.5,
         throwProps: true,
         bounds: "#workspace",
-
         });
     $(function disableLink(){
      $('#imagesLaunch').hide();
@@ -17,4 +16,8 @@ $(document).on('click', '#imagesLaunch', function () {
     $('#imagesLaunch').show();
     });
 
+
+
 });
+
+
